fb7ab8d83aa62beb306692370d2d25f982a7e69a
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-08-22 20:26:51 +0300

    add name and breed property

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,27 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name):
+        self._name = name
+    
+    def get_name (self):
+        return self._name
+    def set_name(self,name):
+        if len(name) <= 25 :
+            self._name = name
+        else :
+            print("Name must be string between 1 and 25 characters.")
+
+    
+    def breed(self):
+        return self._breed
+    
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else :
+            print("Breed must be in list of approved breeds")
+    
+
+    breed = property(set_breed)
+    name = property(get_name,set_name)
